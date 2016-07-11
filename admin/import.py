e2d95f9a224eb87c99a3951e9583a84c30ed06c8
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2016-07-11 19:31:24 +0300

    DENSWXCDIG-10 / first portion of metadata

--- admin/import.py
+++ admin/import.py
@@ -28,6 +28,7 @@ logHandler.setLevel(logging.DEBUG)
 log.addHandler(logHandler)
 
 group_check_lock = RLock()
+org_check_lock = RLock()
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -78,7 +79,6 @@ def action_url(action):
 
 
 class Importer(Thread):
-    semaphore = semaphore
 
     def __init__(self, *args, **kwargs):
         assert 'dataset' in kwargs
@@ -86,11 +86,12 @@ class Importer(Thread):
         super(Importer, self).__init__(*args, **kwargs)
 
     def run(self):
+        log.debug('[{0:^10}] Semaphore lock aquired'.format(self.name))
         try:
             self._import_dataset()
         finally:
-            semaphore.release()
             log.debug('[{0:^10}] Semaphore lock released'.format(self.name))
+            semaphore.release()
 
     def _get_org_id(self, org_name):
         mn = munge_name(org_name)
@@ -106,8 +107,27 @@ class Importer(Thread):
             org_id = org_dict.json()['result']['id']
             org_cache[mn] = org_id
             return org_id
-        log.error(org_dict.content)
-        raise AttributeError('Organization %s not found' % mn)
+        log.error('[{0:^10}] {1}'.format(self.name, org_dict.content))
+        with org_check_lock:
+
+            org = requests.post(
+                action_url('organization_create'),
+                headers={
+                    'Authorization': API_KEY,
+                    'Content-type': 'application/json'
+                },
+                data=dumps({
+                    'title': org_name,
+                    'name': mn
+                })
+            )
+            if org.ok:
+                log.info('[{0:^10}] Organization {1} successfully created'.format(
+                    self.name, org_name))
+            else:
+                log.error('[{0:^10}] Unable to create organization {1}: {2}'.format(
+                    self.name, org_name, org.content))
+        # raise AttributeError('Organization %s not found' % mn)
 
     def _get_language(self, language):
 
@@ -124,12 +144,13 @@ class Importer(Thread):
     def _dataset_to_pkg(self):
         pkg = dict(
             author=self.dataset['publisher'],
-            contact_info=not boolean_validator(
+            contact_info=boolean_validator(
                 self.dataset['expose user contact information'], 0),
             contact_point=self.dataset['contact email'],
             data_model=self.dataset['data models'],
             data_state=self.dataset['data status'].lower(),
-            field_of_research=self.dataset['field of research'],
+            field_of_research=self.dataset['field of research'][
+                self.dataset['field of research'].index(' ')+1:],
             geospatial_topic=self.dataset['geospatial topic'].title().replace(
                 ' And ', ' and '),
             groups=[
@@ -141,19 +162,20 @@ class Importer(Thread):
             license_id=self._get_license(self.dataset['license']),
             name=munge_name(self.dataset['title']),
             notes=self.dataset['description'],
-            owner_org=self._get_org_id(self.dataset['organisation']),
+            owner_org=self._get_org_id(self.dataset.get('organisation', self.dataset['publisher'])),
             private=not boolean_validator(self.dataset['visibility'], 0),
             spatial_coverage=self.dataset['geospatial coverage'],
             tags=[
-                {'name': tag}
+                {'name': munge_name(tag)}
                 for tag in loads(
-                    self.dataset['tags/keywords'].replace("'", '"'))],
+                    self.dataset['tags'].replace("'", '"'))],
             temporal_coverage_from=self.dataset['temporal coverage from'],
             temporal_coverage_to=self.dataset['temporal coverage to'],
             theme=self.dataset['theme'],
             title=self.dataset['title'],
             update_freq=self.dataset['update frequency'].lower()
         )
+        pkg['name'] = pkg['name'][:95] + pkg['id'][:5]
 
         if self.dataset['socrata id']:
             format = 'csv'
@@ -211,7 +233,11 @@ class Importer(Thread):
         #     {'metadata_modified': modified})
         rev = s.query(PackageRevision).filter_by(id=id).order_by(
             PackageRevision.revision_timestamp.asc()).first()
-        rev.revision_timestamp = added
+        # import pdb; pdb.set_trace()
+        if rev:
+            rev.revision_timestamp = added
+        else:
+            log.warn('[{0:^10}] metadata_created not updated'.format(self.name))
 
         s.commit()
         s.close()
@@ -247,6 +273,14 @@ class Importer(Thread):
                 },
                 data=pkg_str
             )
+            req = requests.post(
+                action_url(action),
+                headers={
+                    'Authorization': API_KEY,
+                    'Content-type': 'application/json'
+                },
+                data=pkg_str
+            )
             for resource in resources:
                 resource.pop('url')
                 requests.post(
@@ -256,7 +290,12 @@ class Importer(Thread):
                     files=[upload]
                 )
             self._update_dates(pkg['id'])
-            log.debug(req.content)
+            if req.ok:
+                log.info('[{0:^10}] Success: {1}'.format(
+                    self.name, pkg['name']))
+            else:
+                log.error('[{0:^10}] Failed: {1}'.format(
+                    thread.name, req.content))
 
 
 if not os.path.isfile(args.content):
@@ -266,9 +305,8 @@ with open(args.content, 'r') as content:
     reader = DictReader(content)
     reader.fieldnames = map(str.lower, reader.fieldnames)
 
-    for line in reader:
-        thread = Importer(dataset=line)
-        log.debug('[{0:^10}] Semaphore lock aquired'.format(thread.name))
+    for number, line in enumerate(reader, 1):
         semaphore.acquire()
+        thread = Importer(dataset=line, name="Thread-%d" % number)
         thread.start()
         # break
