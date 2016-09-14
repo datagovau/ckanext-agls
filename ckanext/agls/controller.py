1d9dd917b31af12780bfffbd789db764d135a271
Gregory von Nessi <gvn@Derp-E.local>
2016-09-14 12:10:57 +1000

    - [SXTPDFINXZCB-194]

--- ckanext/agls/controller.py
+++ ckanext/agls/controller.py
@@ -39,16 +39,19 @@ class AGLSController(PackageController):
         if q:
             r = requests.get("http://www.ga.gov.au/gazetteer-search/gazetteer2012/select/?q=id:"+q).json()
 
-            for record in r['response']['docs']:
-                locationParts = record['location'].split(',')
-                latitude = locationParts[0]
-                longitude = locationParts[1]
-                result_dict = {'id': record.get('id'),
-                               'name': record.get('id')+": "+record.get('name'),
-                               'latitude': latitude,
-                               'longitude': longitude,
-                               'geojson': '{"type": "Point","coordinates": ['+ longitude+ ','+latitude+']}'}
-                return result_dict
+            try:
+                for record in r['response']['docs']:
+                    locationParts = record['location'].split(',')
+                    latitude = locationParts[0]
+                    longitude = locationParts[1]
+                    result_dict = {'id': record.get('id'),
+                                   'name': record.get('id')+": "+record.get('name'),
+                                   'latitude': latitude,
+                                   'longitude': longitude,
+                                   'geojson': '{"type": "Point","coordinates": ['+ longitude+ ','+latitude+']}'}
+                    return result_dict
+            except:
+                pass
         return {}
 
 
